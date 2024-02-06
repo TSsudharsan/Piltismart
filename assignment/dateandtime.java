41c290f543b0ea8d2e9211102484836501c68688
TSsudharsan <tssudharsan45@gmail.com>
2024-02-07 00:49:13 +0530

    File Uploaded

--- assignment/dateandtime.java
+++ assignment/dateandtime.java
@@ -10,8 +10,8 @@ public class dateandtime {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter unix time");
         Long unx=sc.nextLong();
-    Date nux=new Date(unx*1000);
-    System.out.println(" Unix time format to Local time format"+ nux.toString());
+        Date nux=new Date(unx*1000);
+        System.out.println(" Unix time format to Local time format "+ nux.toString());
     }
     public static void main(String[]args){
         Scanner s=new Scanner(System.in);
@@ -40,8 +40,8 @@ public class dateandtime {
 
         System.out.println("Enter the number of Days to Add and subtract");
         int asd = s.nextInt();
-        LocalDateTime pd =dt.plusHours(asd);
-        LocalDateTime md =dt.minusHours(asd);
+        LocalDateTime pd =dt.plusDays(asd);
+        LocalDateTime md =dt.minusDays(asd);
         System.out.println("Current Date : "+ dt.format(fdt));
         System.out.println("After Adding "+ asd + " Days:"+ pd.format(fdt));
         System.out.println("After Subtracting "+ asd + " Days:"+ md.format(fdt));
@@ -50,27 +50,28 @@ public class dateandtime {
 
        System.out.println("Enter the number of Month to Add and subtract");
         int asm = s.nextInt();
-        LocalDateTime pm =dt.plusHours(asm);
-        LocalDateTime mm =dt.minusHours(asm);
+        LocalDateTime pm =dt.plusMonths(asm);
+        LocalDateTime mm =dt.minusMonths(asm);
         System.out.println("Current Date : "+ dt.format(fdt));
-        System.out.println("After Adding "+ asm + " Month:"+ pm.format(fdt));
-        System.out.println("After Subtracting "+ asm + " Month:"+ mm.format(fdt));
+        System.out.println("After Adding "+ asm + " Months:"+ pm.format(fdt));
+        System.out.println("After Subtracting "+ asm + " Months:"+ mm.format(fdt));
 
         //5 .Subtract a year from current date
         System.out.println("Enter the number of Year to  subtract");
         int sy = s.nextInt();
         LocalDateTime my =dt.minusYears(sy);
         System.out.println("Current Date : "+ dt.format(fdt));
-        System.out.println("After Subtracting "+ sy + " year : "+ my.format(fdt));
+        System.out.println("After Subtracting "+ sy + " years : "+ my.format(fdt));
 
     //6.Convert current date in to Unix time format
-    Date ux=new Date();
-    Long p= ux.getTime()/1000;
-    System.out.println(" Unix time format "+ p);
+
+        Date ux=new Date();
+        Long p= ux.getTime()/1000;
+        System.out.println(" Current date to Unix time format "+ p);
 
    //7. Java static method to convert local time in to Unix time format 
-   dateandtime obj=new dateandtime();
-   dateandtime.f1();
+        dateandtime obj=new dateandtime();
+        dateandtime.f1();
 
 
 
